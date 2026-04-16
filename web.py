54658fd5aec7175894fd26dc9f46709687dd0eb5
Brett Evans <brettevanssf@gmail.com>
2026-04-16 14:25:31 -0700

    Fix Jinja2Templates call for newer Starlette versions

--- web.py
+++ web.py
@@ -42,7 +42,7 @@ templates = Jinja2Templates(directory=str(BASE / "templates"))
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 
 @app.post("/extract")
